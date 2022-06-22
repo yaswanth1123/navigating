be27cd699e1ff460ff627e929b62d0fcfc8b374a
hanumanthk1128 <khanumanth.msbi@gmail.com>
2022-06-22 18:54:35 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hai(request):
+    return render(request,'hai.html')
+
+
+def hello(request):
+    return render(request,'hello.html')
